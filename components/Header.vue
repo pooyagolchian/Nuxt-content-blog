04896a7b0b53986599fbb138fb8e8ef47f6412b9
Pooya Golchian <pooya.golchian@gmail.com>
2021-01-04 18:03:18 +0330

    update

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header col-12">
+    <div class="container">
+      <div class="row">
+        <div>
+          <change-theme
+            :defaultTheme="'light'"
+            :themeColorList="[
+              'light',
+              'dark',
+              'sepia',
+              'black',
+              'coffee',
+              'rose',
+            ]"
+            :changeThemeOff="true"
+          ></change-theme>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChangeTheme from '../components/Theme'
+export default {
+  components: {
+    'change-theme': ChangeTheme,
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,14 @@
 <template>
   <div>
-    <div class="vue-multiple-themes">
-      <span
-        v-if="changeThemeOff"
-        class="change-theme-box"
-        @click="changeTheme()"
-      >
-        {{ themeName }}
-      </span>
-    </div>
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './../components/Header'
 export default {
-  name: 'Home',
-  data() {
-    return {
-      theme: 'light',
-      counter: 0,
-      themeName: this.defaultTheme,
-      showChangeTheme: this.changeThemeOff,
-      defaultTheme: 'light',
-      themeColorList: ['light', 'dark', 'sepia', 'black', 'coffee', 'rose'],
-      changeThemeOff: true,
-    }
-  },
-  computed: {
-    showChangeThemeHandler() {
-      if (!this.showChangeTheme) {
-        this.theme = this.defaultTheme
-        this.themeName = this.theme
-        this.themeColor(this.theme)
-      }
-    },
-  },
-  methods: {
-    themeColor(themeColor) {
-      this.theme = `${themeColor}`
-      document.title = 'Multiple Themes in Vue.js'
-      const bodyElement = document.body
-      bodyElement.classList.add('app-background')
-      const htmlElement = document.documentElement
-      htmlElement.setAttribute('theme', `${themeColor}`)
-      localStorage.setItem('theme', JSON.stringify(`${themeColor}`))
-    },
-    changeTheme() {
-      this.counter = this.counter + 1
-      if (this.counter === this.themeColorList.length) {
-        this.counter = 0
-      }
-      this.themeName = this.themeColorList[this.counter]
-      this.themeColor(this.themeName)
-    },
-  },
-  mounted() {
-    if (!this.showChangeTheme) this.showChangeThemeHandler
-    if (localStorage.getItem('theme') && this.showChangeTheme) {
-      this.theme = JSON.parse(localStorage.getItem('theme'))
-      this.themeName = this.theme
-      this.themeColor(this.theme)
-    } else {
-      this.theme = this.defaultTheme
-      this.themeName = this.theme
-      this.themeColor(this.theme)
-    }
+  components: {
+    'app-header': AppHeader,
   },
 }
 </script>
