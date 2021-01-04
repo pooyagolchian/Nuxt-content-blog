ee8b308ceb3a1b415e3e771ce83249c1fdaee287
Pooya Golchian <pooya.golchian@gmail.com>
2021-01-04 17:43:29 +0330

    add multiple theme

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,70 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        pooya-blog-en
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div class="vue-multiple-themes">
+      <span
+        v-if="changeThemeOff"
+        class="change-theme-box"
+        @click="changeTheme()"
+      >
+        {{ themeName }}
+      </span>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Home',
+  data() {
+    return {
+      theme: 'light',
+      counter: 0,
+      themeName: this.defaultTheme,
+      showChangeTheme: this.changeThemeOff,
+      defaultTheme: 'light',
+      themeColorList: ['light', 'dark', 'sepia', 'black', 'coffee', 'rose'],
+      changeThemeOff: true,
+    }
+  },
+  computed: {
+    showChangeThemeHandler() {
+      if (!this.showChangeTheme) {
+        this.theme = this.defaultTheme
+        this.themeName = this.theme
+        this.themeColor(this.theme)
+      }
+    },
+  },
+  methods: {
+    themeColor(themeColor) {
+      this.theme = `${themeColor}`
+      document.title = 'Multiple Themes in Vue.js'
+      const bodyElement = document.body
+      bodyElement.classList.add('app-background')
+      const htmlElement = document.documentElement
+      htmlElement.setAttribute('theme', `${themeColor}`)
+      localStorage.setItem('theme', JSON.stringify(`${themeColor}`))
+    },
+    changeTheme() {
+      this.counter = this.counter + 1
+      if (this.counter === this.themeColorList.length) {
+        this.counter = 0
+      }
+      this.themeName = this.themeColorList[this.counter]
+      this.themeColor(this.themeName)
+    },
+  },
+  mounted() {
+    if (!this.showChangeTheme) this.showChangeThemeHandler
+    if (localStorage.getItem('theme') && this.showChangeTheme) {
+      this.theme = JSON.parse(localStorage.getItem('theme'))
+      this.themeName = this.theme
+      this.themeColor(this.theme)
+    } else {
+      this.theme = this.defaultTheme
+      this.themeName = this.theme
+      this.themeColor(this.theme)
+    }
+  },
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
