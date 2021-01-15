ee0c24ac8bb64a761ed2d17d5df346e8a070262d
Pooya Golchian <pooya.golchian@gmail.com>
2021-01-15 23:34:28 +0330

    update and fix route

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,6 @@ export default {
   },
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
-  router: {
-    base: '/pooyagolchian.github.io/',
-  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
