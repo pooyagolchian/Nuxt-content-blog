f6f78d87ebbf133c333debcb2a3033bdbd882abf
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-08-26 21:08:02 +0000

    deploy: 395f701079e66de39c7a055a7fb9c27b4eb54ec3

--- _nuxt/static/1629995108/cv/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629995108",layout:"resume",error:a,serverRendered:true,routePath:"\u002Fcv",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"61a832ca"}}}}(null));
