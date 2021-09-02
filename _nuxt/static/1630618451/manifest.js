5ba178e44c272b928b56ee84e10539304cb362ee
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-09-02 21:36:04 +0000

    deploy: dae8fcd0d41ffc7a2ee1028f6e55b69836323992

--- _nuxt/static/1630618451/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fblog","\u002Fcv","\u002Fblog\u002Faws-ecr-pull-and-extract","\u002Fblog\u002Farangodb-devops","\u002Fblog\u002Fssh-tunnel","\u002Fblog\u002Fconfigure-fcm","\u002Fblog\u002Fvue-multiple-theme","\u002Fblog\u002Fhost-jekyll-theme-on-github"]})
