cd03896dec1ae7d22f3fa552ccbd2e69a1a82c0e
pooya golchian <pooya.golchian@gmail.com>
2021-09-03 03:03:35 +0430

    Update author

--- pages/blog/author/_author.vue
+++ pages/blog/author/_author.vue
@@ -5,23 +5,28 @@
     <h3 class="app-subtitle">
       Here are a list of articles by {{ articles[0].author.name }}:
     </h3>
-    <div class="row pb-40 pt-40">
+    <div class='col-12 p-0'>
+      <app-search-input />
+    </div>
+    <div class='row pb-40'>
       <div
-        class="col-12 col-sm-12 col-md-12 pt-20 pb-20"
-        v-for="article of articles"
-        :key="article.slug"
+        v-for='article of articles'
+        :key='article.slug'
+        class='col-12 col-sm-12 col-md-12 pt-15 pb-15'
       >
         <NuxtLink
-          tag="a"
           :to="{ name: 'blog-slug', params: { slug: article.slug } }"
-          class="link"
+          class='cp mb-20 link'
+          tag='a'
         >
-          <div>
-            <h2 class="app-title fs-19 font-weight-bold pt-10">
+          <div class='w-100'>
+            <h2 class='app-title fs-19 font-weight-normal pt-10'>
               {{ article.title }}
             </h2>
-            <div class="app-subtitle">{{ formatDate(article.date) }}</div>
-            <div class="app-subtitle fa-15 pt-10">
+            <div class='app-subtitle fs-13'>
+              <span class='lnr lnr-calendar-full pr-5'></span>{{ formatDate(article.date) }}
+            </div>
+            <div class='app-subtitle fs-14 fa-15 pt-10'>
               {{ article.description }}
             </div>
           </div>
