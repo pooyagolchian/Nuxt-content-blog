bd139d45115479d4312d22203bea0decee7d04b9
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-09-02 21:20:29 +0000

    deploy: 0b0c81f2416721877423209b68c2c5c1b32d3fa9

--- _nuxt/static/1630617259/blog/vue-multiple-theme/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630617259",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fvue-multiple-theme",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fa053d47"}}}}(null));
