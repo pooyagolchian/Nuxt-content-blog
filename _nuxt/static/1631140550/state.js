e80e1743e5c65f24e0db952247c2249553354c9e
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-09-08 22:35:57 +0000

    deploy: bfd6a838b39ebab6165d36e8ab8fdf607bf84a50

---
+++ _nuxt/static/1631140550/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631140550",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a03d2744"}}}}(null,"\u002F"));
