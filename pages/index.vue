0f3a99b4239a08300a2a9814e6f8991c0ce71c19
pooya golchian <pooya.golchian@gmail.com>
2021-09-08 22:33:10 +0430

    Update design

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,46 @@
 <template>
   <div class='row m-0'>
+    <div class='col-12 col-sm-12 col-md-4 info'>
+      <div class='row pb-30'>
+        <div class='col-12 col-sm-12 col-md-12'>
+          <nuxt-link to='/about' tag='div'>
+            <img
+              alt=''
+              class='h1-00 w-100 pb-20 pt-20 my-photo cp'
+              src='/img/home-img/pooya-golchian.JPG'
+            />
+          </nuxt-link>
+        </div>
+        <nuxt-link to='/about' tag='div'
+                   class='col-12 col-sm-12 col-md-12 d-flex justify-content-center align-items-center flex-column cp'
+        >
+          <h1
+            class='fs-18 app-title text-left d-flex flex-1 w-100 font-weight-bold'
+          >
+            I'm Pooya Golchian
+          </h1>
+          <ul
+            class='p-0 m-0 d-flex justify-content-center align-items-start flex-column flex-1 w-100 app-subtitle'
+          >
+            <li>Frontend Engineer (Vue.js/React.js)</li>
+            <li>DevOps Engineer and Linux Server Engineer</li>
+            <li>Code, Coffee, Crypto</li>
+          </ul>
+          <p class='fs-14 app-title pt-10'>
+            I was born in Tehran in 1990. When my father bought a computer for our home, I fell in love with it. I start
+            to learned Linux with Ubuntu and Fedora when I was 16 years old. Now, I have a master's degree in computer
+            engineering. I have 10 years' experience in web development. I have been using Javascript for web
+            development. I have 4 years of experience in Vue.js and, Nuxt.js. I worked with React.js too. I have
+            developed lots of web applications with high-tech frameworks. I have experience with PHP, WordPress, Drupal,
+            headless CMS, and JAM STACK architecture. For more information about my professional full-time work, please
+            see my
+            <nuxt-link to='/cv'>CV</nuxt-link>
+            .
+          </p>
+        </nuxt-link>
+      </div>
+
+    </div>
     <div class='col-12 col-sm-12 col-md-5 blog'>
 
       <div class='col-12 p-0'>
@@ -29,47 +70,6 @@
             </div>
           </NuxtLink>
         </div>
-      </div>
-
-    </div>
-    <div class='col-12 col-sm-12 col-md-4 info'>
-      <div class='row pb-30'>
-        <div class='col-12 col-sm-12 col-md-12'>
-          <nuxt-link to='/about' tag='div'>
-            <img
-              alt=''
-              class='h1-00 w-100 pb-20 pt-20 my-photo cp'
-              src='/img/home-img/pooya-golchian.JPG'
-            />
-          </nuxt-link>
-        </div>
-        <nuxt-link to='/about' tag='div'
-          class='col-12 col-sm-12 col-md-12 d-flex justify-content-center align-items-center flex-column cp'
-        >
-          <h1
-            class='fs-18 app-title text-left d-flex flex-1 w-100 font-weight-bold'
-          >
-            I'm Pooya Golchian
-          </h1>
-          <ul
-            class='p-0 m-0 d-flex justify-content-center align-items-start flex-column flex-1 w-100 app-subtitle'
-          >
-            <li>Frontend Engineer (Vue.js/React.js)</li>
-            <li>DevOps Engineer and Linux Server Engineer</li>
-            <li>Code, Coffee, Crypto</li>
-          </ul>
-          <p class='fs-14 app-title pt-10'>
-            I was born in Tehran in 1990. When my father bought a computer for our home, I fell in love with it. I start
-            to learned Linux with Ubuntu and Fedora when I was 16 years old. Now, I have a master's degree in computer
-            engineering. I have 10 years' experience in web development. I have been using Javascript for web
-            development. I have 4 years of experience in Vue.js and, Nuxt.js. I worked with React.js too. I have
-            developed lots of web applications with high-tech frameworks. I have experience with PHP, WordPress, Drupal,
-            headless CMS, and JAM STACK architecture. For more information about my professional full-time work, please
-            see my
-            <nuxt-link to='/cv'>CV</nuxt-link>
-            .
-          </p>
-        </nuxt-link>
       </div>
 
     </div>
