e24e9d83121f4e9ff41fb92f5cb0e6952f381617
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-09-08 17:59:11 +0000

    deploy: 4fcb5098e77e2f1a66aba4e07a6ee62d212e0ef0

--- _nuxt/static/1631123589/blog/aws-ecr-pull-and-extract/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631123589",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Faws-ecr-pull-and-extract",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"01a11512"}}}}(null));
