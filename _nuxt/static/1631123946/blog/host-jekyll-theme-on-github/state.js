ce08d5cfeb49f68c653b5c8298904dd03c7a2a2e
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-09-08 18:05:16 +0000

    deploy: 0f3a99b4239a08300a2a9814e6f8991c0ce71c19

--- _nuxt/static/1631123946/blog/host-jekyll-theme-on-github/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631123946",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhost-jekyll-theme-on-github",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"96ae9652"}}}}(null));
---
+++ _nuxt/static/1631124311/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631124311",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ed5eedf2"}}}}(null,"\u002F"));
