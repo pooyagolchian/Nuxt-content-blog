f9a07f3616c4138fefff02d53b78402eac086b47
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-09-09 13:57:34 +0000

    deploy: da4c58deb8f723b7205983433543dfa0a25f518b

--- _nuxt/static/1631140597/blog/ssh-tunnel/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631140597",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fssh-tunnel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a2d2e04d"}}}}(null));
--- _nuxt/static/1631140597/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631140597",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a2d2e04d"}}}}(null,"\u002F"));
