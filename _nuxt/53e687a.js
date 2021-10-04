fb68ce4b18613a6e328fd60b0def0c4903bd1f99
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-10-04 05:22:39 +0000

    deploy: 5f9cbb7e03caa30b0f0e1a1fe6743c4e22453c91

---
+++ _nuxt/53e687a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{327:function(n,t,e){"use strict";e.r(t);var l=e(11),component=Object(l.a)({},(function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[e("p",[n._t("info-box",(function(){return[n._v("default")]}))],2)])}),[],!1,null,null,null);t.default=component.exports}}]);
