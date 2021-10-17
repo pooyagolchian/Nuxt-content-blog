a52493b7715bc3e0f463c491915774088c76dadc
pooya golchian <pooya.golchian@gmail.com>
2021-10-17 21:56:24 +0330

    config: update config and compress all js assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
+  ssr: false,
   target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
@@ -7,7 +8,7 @@ export default {
     titleTemplate: 'Pooya Golchian | %s',
     title: 'Software Engineer, Frontend developer, DevOps Engineer',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
@@ -16,7 +17,7 @@ export default {
         hid: 'description',
         name: 'description',
         content:
-          'pooya golchian, pooyagolcian, pooya, golchian, fontend developer, frontend engineer, Web Developer, Javascript Developer, Vue.js Developer, Nuxt.js Developer, React.js Developer'
+          'pooya golchian, pooyagolcian, pooya, golchian, fontend developer, frontend engineer, Web Developer, Javascript Developer, Vue.js Developer, Nuxt.js Developer, React.js Developer',
       },
       { property: 'og:site_name', content: 'Pooya Golchian' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
@@ -24,33 +25,37 @@ export default {
       {
         hid: 'og:url',
         property: 'og:url',
-        content: 'https://pooyagolchian.com'
+        content: 'https://pooyagolchian.com',
       },
       {
         hid: 'og:title',
         property: 'og:title',
-        content: 'Pooya Golchian | Software Engineer'
-      }
+        content: 'Pooya Golchian | Software Engineer',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: ['~/assets/css/main'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~plugins/ga.js', mode: 'client' }],
+  plugins: [
+    { src: '~plugins/ga.js', mode: 'client' },
+    '~plugins/preview.client.js',
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: [
     {
       path: '~/components',
-      pathPrefix: false
-    }
+      pathPrefix: false,
+    },
   ],
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
+    'nuxt-compress',
     '@nuxtjs/google-analytics',
     '@nuxtjs/google-fonts',
     [
@@ -65,31 +70,31 @@ export default {
               'faMapMarkerAlt',
               'faGlobe',
               'faMobileAlt',
-              'faInbox'
-            ]
+              'faInbox',
+            ],
           },
           {
             set: '@fortawesome/free-brands-svg-icons',
-            icons: ['faGithub', 'faLinkedin']
+            icons: ['faGithub', 'faLinkedin'],
           },
           {
             set: '@fortawesome/free-regular-svg-icons',
-            icons: ['faLightbulb']
-          }
-        ]
-      }
-    ]
+            icons: ['faLightbulb'],
+          },
+        ],
+      },
+    ],
   ],
 
   googleFonts: {
     families: {
-      Poppins: [300, 400, 500, 600, 700]
+      Poppins: [300, 400, 500, 600, 700],
     },
-    display: 'swap'
+    display: 'swap',
   },
 
   googleAnalytics: {
-    id: 'UA-205412246-1'
+    id: 'UA-205412246-1',
   },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -98,8 +103,8 @@ export default {
       '@netsells/nuxt-hotjar',
       {
         id: '2560666',
-        sv: '6'
-      }
+        sv: '6',
+      },
     ],
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
@@ -108,38 +113,38 @@ export default {
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
     '@nuxtjs/style-resources',
-    '@nuxtjs/feed'
+    '@nuxtjs/feed',
   ],
   pwa: {
     meta: {
       title: 'Pooya Golchian | Software Engineer',
-      author: 'Pooya Golchian'
+      author: 'Pooya Golchian',
     },
     manifest: {
       name: 'Pooya Golchian Technical Blog',
       short_name: 'Pooya Golchian Blog',
       lang: 'en',
-      description: 'Pooya Golchian Personal Website'
+      description: 'Pooya Golchian Personal Website',
     },
     icon: {
       fileName: 'icon.png',
-      source: '/static/icon.png'
-    }
+      source: '/static/icon.png',
+    },
   },
   feed() {
     const baseUrlArticles = '';
     const baseLinkFeedArticles = '/blog';
     const feedFormats = {
       rss: { type: 'rss2', file: 'rss.xml' },
-      json: { type: 'json1', file: 'feed.json' }
+      json: { type: 'json1', file: 'feed.json' },
     };
     const { $content } = require('@nuxt/content');
 
-    const createFeedArticles = async function(feed) {
+    const createFeedArticles = async function (feed) {
       feed.options = {
         title: 'Pooya Golchian | Software Engineer',
-        description: 'I\'m Frontend developer and DevOps engineer',
-        link: baseUrlArticles
+        description: "I'm Frontend developer and DevOps engineer",
+        link: baseUrlArticles,
       };
       const articles = await $content('articles').fetch();
       articles.forEach((article) => {
@@ -151,7 +156,7 @@ export default {
           date: article.published,
           description: article.description,
           content: article.description,
-          author: article.author.name
+          author: article.author.name,
         });
       });
     };
@@ -159,7 +164,7 @@ export default {
     return Object.values(feedFormats).map(({ file, type }) => ({
       path: `${baseLinkFeedArticles}/${file}`,
       type: type,
-      create: createFeedArticles
+      create: createFeedArticles,
     }));
   },
 
@@ -170,21 +175,21 @@ export default {
   content: {
     markdown: {
       prism: {
-        theme: 'prism-themes/themes/prism-material-oceanic.css'
-      }
+        theme: 'prism-themes/themes/prism-material-oceanic.css',
+      },
     },
-    nestedProperties: ['author.name']
+    nestedProperties: ['author.name'],
   },
   generate: {
     fallback: true,
-    exclude: [/code/, /^(?=.*\btest\b).*$/]
+    exclude: [/code/, /^(?=.*\btest\b).*$/],
+    crawler: false,
   },
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
   },
 
-
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {}
+  build: {},
 };
