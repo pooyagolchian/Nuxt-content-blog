3ec4d3dcacb9e9549402569ff486ea6947256d3b
pooyagolchian <pooyagolchian@users.noreply.github.com>
2021-11-25 14:00:59 +0000

    deploy: 3b43c0d951356d634e73aadace87599b0f507aec

---
+++ _nuxt/static/1637848855/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637848855",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"59f9e9d8"}}}}(null));
---
+++ _nuxt/static/1637848855/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637848855",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"59f9e9d8"}}}}(null,"\u002F"));
