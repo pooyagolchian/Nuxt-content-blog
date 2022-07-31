53eb49e509ab963bd37e6c58ae8be5470a3d56b8
Pooya Golchian <pooya.golchian@gmail.com>
2022-07-31 22:21:00 +0430

    update

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -3,34 +3,29 @@
     <div class="container">
       <div class="row pb-30">
         <div
-          class="
-            col-12 col-sm-12
-            justify-content-center
-            text-left
-            d-flex
-            flex-column
-            p-0
-          "
+          class="col-12 col-sm-12 justify-content-center text-left d-flex flex-column p-0"
         >
           <h1 class="fs-18 app-title text-left font-weight-bold pt-10">
             I'm Pooya Golchian
           </h1>
           <ul class="pb-10 app-subtitle">
-            <li>Senior Frontend Developer (Vue.js/React.js)</li>
+            <li>Senior Frontend Developer (React.js/Vue.js)</li>
             <li>Code, Coffee, Crypto</li>
           </ul>
           <p class="fs-14 app-title">
-            I was born in Tehran in 1990. When my father bought a computer for
-            our home, I fell in love with it. I start to learned Linux with
-            Ubuntu and Fedora when I was 16 years old. Now, I have a master's
-            degree in computer engineering. I have 10 years' experience in web
-            development. I have been using Javascript for web development. I
-            have five years of experience in Vue.js and, Nuxt.js. I worked with
-            React.js too. I have developed lots of web applications with
-            high-tech frameworks. I have experience with PHP, WordPress, Drupal,
-            headless CMS, and JAMSTACK architecture. For more information about
-            my professional full-time work.
-           
+            I have worked as a front-end developer since 2012. I like to
+            transfer the design to code. Web application architecture, UX, and
+            front-end are my favorite things that I enjoyed in my work career.
+            Moreover, problem-solving, communication, and leadership that other
+            skills I have been developing in five recent years. I have worked
+            with different startup and organization software engineering
+            companies in Iran. Close communication with the product team and
+            working with scrum methodology during these years made me agile and
+            flexible for the different circumstances that every application
+            would be in it. In addition, I have worked with DevOps tools. I'm an
+            engineer at my heart. On top of that, I'd love javascript to the
+            bone. I like to be in an international career to improve my work
+            environment and expand my knowledge horizon.
           </p>
         </div>
       </div>
