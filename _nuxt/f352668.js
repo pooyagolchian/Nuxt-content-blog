3d1020776acc1bc98ddd1ed666cde12a3543e347
pooyagolchian <pooyagolchian@users.noreply.github.com>
2023-03-31 21:45:04 +0000

    deploy: d618f9667f910a667e2d98ba20c129eda9596152

--- _nuxt/f352668.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{377:function(n,t,e){"use strict";e.r(t);var o=e(18),component=Object(o.a)({},(function(){var n=this,t=n._self._c;return t("div",[t("p",[n._t("info-box",(function(){return[n._v("default")]}))],2)])}),[],!1,null,null,null);t.default=component.exports}}]);
